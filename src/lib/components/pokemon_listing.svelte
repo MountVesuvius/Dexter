7866d93d87739727a916d53716e25887edaaa044
MountVesuvius <JudeALakkis@gmail.com>
2023-10-04 23:51:33 +1100

    Updates to handle api

--- src/lib/components/pokemon_listing.svelte
+++ src/lib/components/pokemon_listing.svelte
@@ -2,11 +2,13 @@
     import Types from "./types.svelte";
     export let name = "MissingNo.";
     export let types = ['flying', 'normal'];
+    export let id = 1;
 </script>
 
 <div class="w-full h-16 flex justify-between items-center p-2 border-b">
     <div class="flex justify-center text-center items-center space-x-2">
-        <div class="h-8 w-8 border border-black border-dashed rounded-full"/>
+        <!-- <div class="h-8 w-8 border border-black border-dashed rounded-full"/> -->
+        <img class="h-14 w-14" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png" alt="">
         <span class="capitalize">{name}</span>
     </div>
     <div class="flex">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,15 +7,15 @@
 
 <div class="h-screen flex flex-col">
     <header class="bg-dexter">
-        <div class="h-16 w-full text-white">
-            this is header content
+        <div class="h-16 w-full text-white text-center flex justify-center items-center">
+            <span class="capitalize font-mono">All Pokemon</span>
         </div>
     </header>
     <main class="flex-grow overflow-auto">
         {#each pokeData as pokemon}
             <!-- <a href={"https://pokeapi.co/api/v2/pokemon/" + pokemon.identifier}> -->
-            <a href={"/pokemon/" + pokemon.identifier}>
-                <PokemonListing name={pokemon.identifier} types={pokemon.types} />
+            <a href={"/pokemon/" + pokemon.id}>
+                <PokemonListing id={pokemon.id} name={pokemon.identifier} types={pokemon.types} />
             </a>
         {/each}
     </main>
