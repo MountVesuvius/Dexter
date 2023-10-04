45b78e0c00812d04776f0e41ad020e8489291116
MountVesuvius <JudeALakkis@gmail.com>
2023-10-04 23:09:50 +1100

    Added source colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'dexter': '#D25353',
+        'grass': '#7AB15B',
+        'poison': '#7F4382',
+        'fire': '#E0854C',
+        'flying': '#9789E5',
+        'water': '#6681D9',
+        'bug': '#9DA942',
+        'dark': '#554B3B',
+        'ground':'#D9C077',
+        'rock': '#B29E50',
+        'ice':'#9ECACD',
+        'electric': '#F0CD5D',
+        'dragon': '#6944EB',
+        'fighting': '#B73A3A',
+        'steel': '#BBBDD0',
+        'ghost': '#584F76',
+        'normal': '#939565',
+        'fairy': '#D7A2E2',
+        'psychic' : '#DE5B73'
+      }
+    }
   },
   plugins: []
 };
