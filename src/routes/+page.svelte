2fd22f045c0b1f15d69b34d5775ccbe9d652cc3a
MountVesuvius <JudeALakkis@gmail.com>
2023-10-04 23:12:28 +1100

    Home page listing created

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    // import pokeData from "$lib/pokeData.json";
+    import pokeData from "$lib/sample_data.json";
+
+    import PokemonListing from "$lib/components/pokemon_listing.svelte";
+</script>
+
+<div class="h-screen flex flex-col">
+    <header class="bg-dexter">
+        <div class="h-16 w-full text-white">
+            this is header content
+        </div>
+    </header>
+    <main class="flex-grow overflow-auto">
+        {#each pokeData as pokemon}
+            <!-- <a href={"https://pokeapi.co/api/v2/pokemon/" + pokemon.identifier}> -->
+            <a href={"/pokemon/" + pokemon.identifier}>
+                <PokemonListing name={pokemon.identifier} types={pokemon.types} />
+            </a>
+        {/each}
+    </main>
+</div>
