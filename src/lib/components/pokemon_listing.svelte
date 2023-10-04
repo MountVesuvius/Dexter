0d43491a2672e5421d7be230536a83c09e39ce9e
MountVesuvius <JudeALakkis@gmail.com>
2023-10-04 23:11:26 +1100

    List item, implements type indicator

---
+++ src/lib/components/pokemon_listing.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Types from "./types.svelte";
+    export let name = "MissingNo.";
+    export let types = ['flying', 'normal'];
+</script>
+
+<div class="w-full h-16 flex justify-between items-center p-2 border-b">
+    <div class="flex justify-center text-center items-center space-x-2">
+        <div class="h-8 w-8 border border-black border-dashed rounded-full"/>
+        <span class="capitalize">{name}</span>
+    </div>
+    <div class="flex">
+        {#each types as type}
+            <Types type={type}/>
+        {/each}
+    </div>
+</div>
