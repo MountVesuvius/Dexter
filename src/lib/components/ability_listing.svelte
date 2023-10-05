22e48f29dcf6ec5da030f8fb498566ecb5103cf9
MountVesuvius <JudeALakkis@gmail.com>
2023-10-05 20:24:27 +1100

    Basic ability listing component added

---
+++ src/lib/components/ability_listing.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let name = "overgrow";
+    export let effect = "Strengthens grass moves to inflict 1.5x damage at 1/3 max hp or less";
+</script>
+
+<div class="w-full flex flex-col justify-between items-start font-mono">
+    <h3 class="uppercase text-lg">{name}</h3>
+    <p class="text-sm">{effect}</p>
+</div>
